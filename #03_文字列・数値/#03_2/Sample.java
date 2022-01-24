93a10f72482261864f045671d48f397a0d39f803
Masahiro Toba <86113516+masahirotoba@users.noreply.github.com>
2022-01-24 18:22:26 +0900

    Add files via upload

---
+++ #03_文字列・数値/#03_2/Sample.java
@@ -0,0 +1,7 @@
+public class Sample{
+    public static void main(String[] args){
+        System.out.println("HelloWorld");
+        System.out.println("1000");
+        System.out.println("1000円");
+    }
+}
---
+++ #03_文字列・数値/#03_4/Sample.java
@@ -0,0 +1,6 @@
+public class Sample{
+    public static void main(String[] args){
+        System.out.println(1000 + "円");
+        System.out.println("金額：" + (1000 + 500));
+    }
+}
